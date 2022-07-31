367a3c2fda2d50eb4d5bc254ebe9f81b7cc0e010
dosorio55 <dosorio55@gmail.com>
2022-07-31 22:16:42 +0200

    changed BASE url server

--- src/context/api/context.js
+++ src/context/api/context.js
@@ -1,7 +1,3 @@
-// export const
-
-
-
-export const BASE_URL = "https://windview-server.herokuapp.com"
+export const BASE_URL = "https://windview-node.herokuapp.com/"
 // export const BASE_URL = "http://localhost:4000"
 
