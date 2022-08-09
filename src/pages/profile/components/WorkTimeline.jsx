994051abfdf47eacc6c737faf8d7a08f87738a9a
dosorio55 <dosorio55@gmail.com>
2022-08-09 09:01:38 +0200

    arrow timeline working

--- src/pages/profile/components/WorkTimeline.jsx
+++ src/pages/profile/components/WorkTimeline.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Box, Divider, styled, Typography } from '@mui/material';
+import "./ArrowTimeline.scss"
 
 const GridContainer = styled(Box)(({
     display: "grid",
-    gridTemplateColumns: '3fr .2fr 6fr',
+    gridTemplateColumns: '4fr .3fr 6fr',
     gap: 2
 }));
 
-export const StyledBox = styled(Box)(({ theme }) => ({
+/* export const StyledBox = styled(Box)(({ theme }) => ({
     borderRadius: theme.shape.borderRadius,
     backgroundColor: theme.palette.primary.main,
     display: "flex",
@@ -15,16 +16,16 @@ export const StyledBox = styled(Box)(({ theme }) => ({
     justifyContent: 'center',
     padding: 3,
     height: "1rem"
-}));
+})); */
 
 const WorkTimeline = () => {
     return (
         <GridContainer>
-            <StyledBox>
+            <div className='arrow'>
                 <Typography color="secondary">
                     2022
                 </Typography>
-            </StyledBox>
+            </div>
             <Divider orientation="vertical" />
             <Box sx={{ px: 1 }}>
                 <Typography component={'span'} variant={'body2'}>
